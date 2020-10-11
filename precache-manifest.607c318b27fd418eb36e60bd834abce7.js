b27c9a6bca9f452bc44627285ed4929e2579eff8
maulana10de <maulana10de@gmail.com>
2020-10-11 14:12:19 +0700

    Updates

---
+++ precache-manifest.607c318b27fd418eb36e60bd834abce7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffdd8ce09fc1eaf6725eb9a6839640be",
+    "url": "/Ujian_FrontEnd_Ade_Maulana_JCWMAH0102/index.html"
+  },
+  {
+    "revision": "74dbe02bb4c3e0357328",
+    "url": "/Ujian_FrontEnd_Ade_Maulana_JCWMAH0102/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e4fdfc10bf9f13bcaa1c",
+    "url": "/Ujian_FrontEnd_Ade_Maulana_JCWMAH0102/static/css/main.e5f2a41f.chunk.css"
+  },
+  {
+    "revision": "74dbe02bb4c3e0357328",
+    "url": "/Ujian_FrontEnd_Ade_Maulana_JCWMAH0102/static/js/2.025eed40.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/Ujian_FrontEnd_Ade_Maulana_JCWMAH0102/static/js/2.025eed40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4fdfc10bf9f13bcaa1c",
+    "url": "/Ujian_FrontEnd_Ade_Maulana_JCWMAH0102/static/js/main.330ec4eb.chunk.js"
+  },
+  {
+    "revision": "2ff1f3f26e0d7590eaee",
+    "url": "/Ujian_FrontEnd_Ade_Maulana_JCWMAH0102/static/js/runtime-main.b85da0d9.js"
+  },
+  {
+    "revision": "af5969b3a3bf11d38a9975a2016afb13",
+    "url": "/Ujian_FrontEnd_Ade_Maulana_JCWMAH0102/static/media/logo.af5969b3.png"
+  },
+  {
+    "revision": "daf535aba703817397885b5b880225e9",
+    "url": "/Ujian_FrontEnd_Ade_Maulana_JCWMAH0102/static/media/small-banner.daf535ab.jpg"
+  }
+]);
